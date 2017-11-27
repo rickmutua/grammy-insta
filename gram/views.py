3a6689f853110ff4eb9bf5695d4a40b2d8736abb
Erick <rick.mutua12@gmail.com>
2017-11-27 23:19:20 +0300

    added and integrated django authentication system

--- gram/views.py
+++ gram/views.py
@@ -3,9 +3,10 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
 @login_required(login_url='/accounts/login')
 def index(request):
 
     title = 'Grammy Insta'
 
-    return render(request, 'index', {'title': title })
+    return render(request, 'base/index.html', {'title': title })
