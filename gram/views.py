7639be18895c634f8cad09fdd26a657a9796e3fb
Erick <rick.mutua12@gmail.com>
2017-11-27 22:24:09 +0300

    defined index page

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login')
+def index(request):
+
+    title = 'Grammy Insta'
+
+    return render(request, 'index', {'title': title })
