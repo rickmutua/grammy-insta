2cd9edded46cfab01fd02f38e3e407a0922cbf86
Erick <rick.mutua12@gmail.com>
2017-11-27 23:41:28 +0300

    created user profile forms to update user info

---
+++ gram/forms.py
@@ -0,0 +1,21 @@
+from .models import Profile
+from django.contrib.auth.models import User
+from django import forms
+
+
+class UserForm(forms.ModelForm):
+
+    class Meta:
+
+        model = User
+
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileForm(forms.ModelForm):
+
+    class Meta:
+
+        model = Profile
+
+        fields = ('bio', 'gender')
