4b028e469ab30b4303d58ca8c07036d152f82ac8
Erick <rick.mutua12@gmail.com>
2017-11-27 20:50:47 +0300

    created, set up and configured database

--- insta/settings.py
+++ insta/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'insta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'insta',
+        'USER': 'erick',
+        'PASSWORD':'qwerty12345',
     }
 }
 
