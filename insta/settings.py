c2fe5bc2f9782f276bad57aeca191797f18d266c
Erick <rick.mutua12@gmail.com>
2017-12-08 09:09:32 +0300

    deployment to heroku

--- insta/settings.py
+++ insta/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['python-grammy.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
